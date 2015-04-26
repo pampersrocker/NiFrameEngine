d350d848d52a16ba9a0905a7d5cbfeb1bf64ac62
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-04-26 12:06:26 +0200

    Renamed h files to hpp files

--- NiFrameEngine/NiFrameD3D11/src/NiFrameD3D11.cpp
+++ NiFrameEngine/NiFrameD3D11/src/NiFrameD3D11.cpp
@@ -1,5 +1,20 @@
 // NiFrameD3D11.cpp : Defines the exported functions for the DLL application.
 //
 
-#include "stdafx.h"
-#include "NiFrameD3D11.h"
+#include "stdafx.hpp"
+#include <NFEnginePCH.hpp>
+#include <NFRenderDevice.hpp>
+#include "NiFrameD3D11.hpp"
+
+extern "C"
+{
+  void __declspec( dllexport ) CreateRenderDevice( nfe::RenderDevice** renderDevice)
+  {
+
+  }
+
+  void __declspec( dllexport ) ReleaseRenderDevice( nfe::RenderDevice** renderDevice )
+  {
+
+  }
+}
