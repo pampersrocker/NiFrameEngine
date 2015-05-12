747f9a5d3b6de209bea63c5c8bebc939b7e3b5b8
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-05-12 13:00:21 +0200

    Added implementation for BlockAllocator

--- NiFrameEngine/NiFrameEngine/inc/Memory/NFBlockAllocator.hpp
+++ NiFrameEngine/NiFrameEngine/inc/Memory/NFBlockAllocator.hpp
@@ -15,11 +15,30 @@ namespace nfe
     WorstFit
   };
 
-  class BlockAllocator : IAllocator
+  struct NIFRAME_DLL_EXPORT BlockAllocatorChunk
+  {
+    uint8* Pointer;
+    uint8* EndAddress;
+    uint64 Size;
+
+    void CalculateEndAddress();
+
+    bool Valid();
+
+    bool operator ==( const BlockAllocatorChunk& chunk ) const;
+    bool operator <= ( const BlockAllocatorChunk& chunk ) const;
+    bool operator >= ( const BlockAllocatorChunk& chunk ) const;
+    bool operator < ( const BlockAllocatorChunk& chunk ) const;
+    bool operator > ( const BlockAllocatorChunk& chunk ) const;
+
+  };
+
+  class NIFRAME_DLL_EXPORT BlockAllocator : IAllocator
   {
   public:
     BlockAllocator(
       const char* name = "NFBlockAllocator",
+      uint32 alignment = 4U,
       BlockAllocatorType allocationType = BlockAllocatorType::FirstFit,
       uint64 initialAllocationSize = 0U,
       IAllocator* parentAllocator = nullptr);
@@ -34,10 +53,31 @@ namespace nfe
 
 
   private:
+
+    uint64 FindFirstFitBlock( uint64 size );
+    uint64 FindBestFitBlock( uint64 size );
+    uint64 FindWorstFitBlock( uint64 size );
+
+    bool TryMergeBlocks( const BlockAllocatorChunk& chunk );
+
+    /**
+    @brief Splits the firstHalf into two blocks if there is space left for a second block, otherwise the secondHalf is set to nullptr
+
+    It is expected that the firstHalf has enough memory for the size
+
+    @param size The size of needed chunk
+    @param BlockAllocatorChunk & firstHalf Serves first a input of the block to split, will be modified if the block needs to be splitted
+    @param BlockAllocatorChunk & secondHalf Output if the block is splitted, otherwise set to nullptr
+    */
+    void SplitBlock( uint64 size, BlockAllocatorChunk& firstHalf, BlockAllocatorChunk& secondHalf );
+
+    uint32 m_Alignment;
+
     IAllocator* m_ParentAllocator;
     BlockAllocatorType m_AllocatorType;
-
-    Vector<uint8*> m_MemoryChunks;
+    Vector<BlockAllocatorChunk> m_AllocatedBlocks;
+    Vector<BlockAllocatorChunk> m_FreeMemoryChunks;
+    Vector<BlockAllocatorChunk> m_UsedMemoryChunks;
   };
 
 }
--- NiFrameEngine/NiFrameEngine/src/Memory/NFBlockAllocator.cpp
+++ NiFrameEngine/NiFrameEngine/src/Memory/NFBlockAllocator.cpp
@@ -6,18 +6,34 @@ using namespace nfe;
 
 nfe::BlockAllocator::BlockAllocator(
   const char* name /*= "NFBlockAllocator"*/,
+  uint32 alignment,
   BlockAllocatorType allocationType /*= BlockAllocatorType::FirstFit*/,
   uint64 initialAllocationSize /*= 0U*/,
   IAllocator* parentAllocator /*= nullptr*/ ) :
   IAllocator(name),
   m_ParentAllocator(parentAllocator),
-  m_AllocatorType(allocationType)
+  m_AllocatorType(allocationType),
+  m_Alignment(alignment),
+  m_FreeMemoryChunks( parentAllocator ),
+  m_UsedMemoryChunks( parentAllocator ),
+  m_AllocatedBlocks( parentAllocator )
 {
   if (m_ParentAllocator == nullptr)
   {
     m_ParentAllocator = GetDefaultAllocator();
   }
-  m_MemoryChunks = Vector<uint8*>( m_ParentAllocator );
+  if( initialAllocationSize > 0 )
+  {
+    BlockAllocatorChunk initalBlock;
+    uint64 alignedSize = ::nfe::alignedSize( initialAllocationSize, alignment );
+
+    uint8* ptr = static_cast< uint8* >( m_ParentAllocator->Allocate( alignedSize ) );
+    initalBlock.Pointer = ptr;
+    initalBlock.Size = alignedSize;
+    initalBlock.CalculateEndAddress();
+    m_AllocatedBlocks.Add( initalBlock );
+    m_FreeMemoryChunks.Add( initalBlock );
+  }
 }
 
 nfe::BlockAllocator::~BlockAllocator()
@@ -37,11 +53,271 @@ nfe::BlockAllocatorType nfe::BlockAllocator::GetAllocatorType() const
 
 void* nfe::BlockAllocator::Allocate( uint64 size )
 {
-  NF_ASSERT( false, "Not Implemented" );
-  return nullptr;
+  NF_ASSERT( size > 0, "One cannot simply allocate a size of 0!" );
+  uint64 blockIdx = -1;
+  uint64 alignedSize = ::nfe::alignedSize( size );
+  switch (m_AllocatorType)
+  {
+  case BlockAllocatorType::BestFit:
+    blockIdx = FindBestFitBlock( alignedSize );
+    break;
+  case BlockAllocatorType::FirstFit:
+    blockIdx = FindFirstFitBlock( alignedSize );
+    break;
+  case BlockAllocatorType::WorstFit:
+    blockIdx = FindWorstFitBlock( alignedSize );
+    break;
+  default:
+    NF_ASSERT( false, "?!" );
+    break;
+  }
+
+  if (blockIdx == -1)
+  {
+    uint64 newBlockSize = 0;
+
+    if (m_AllocatedBlocks.Size() > 0)
+    {
+      auto& lastBlock = m_AllocatedBlocks[ m_AllocatedBlocks.Size() - 1 ];
+      newBlockSize = alignedSize > lastBlock.Size * 2 ? alignedSize : lastBlock.Size * 2;
+    }
+    else
+    {
+      newBlockSize = alignedSize;
+    }
+    BlockAllocatorChunk newBlock;
+    newBlock.Pointer = static_cast< uint8* >( m_ParentAllocator->Allocate( newBlockSize ) );
+    newBlock.Size = newBlockSize;
+    newBlock.CalculateEndAddress();
+    m_AllocatedBlocks.Add( newBlock );
+    blockIdx = m_FreeMemoryChunks.Size();
+    m_FreeMemoryChunks.Add( newBlock );
+  }
+  BlockAllocatorChunk block = m_FreeMemoryChunks[ blockIdx ];
+  BlockAllocatorChunk secondChunk;
+  SplitBlock( alignedSize, block, secondChunk );
+  m_UsedMemoryChunks.Add( block );
+  if( secondChunk.Valid() )
+  {
+    m_FreeMemoryChunks[ blockIdx ] = secondChunk;
+  }
+  else
+  {
+    m_FreeMemoryChunks.RemoveAt( blockIdx );
+  }
+
+  uint8 offset = static_cast< uint8 >( m_Alignment- reinterpret_cast< uintptr_t >( block.Pointer ) % m_Alignment );
+  uint8* memory = block.Pointer;
+  memory += offset;
+  memory[ -1 ] = offset;
+
+  return memory;
 }
 
 void nfe::BlockAllocator::Deallocate( void* address )
 {
-  NF_ASSERT( false, "Not implemented" );
+  uint8 offset = static_cast< uint8* >( address )[ -1 ];
+  uint8* pointer = static_cast< uint8* >( address ) -offset;
+
+  uint64 idx = 0;
+  for( auto& chunk : m_UsedMemoryChunks )
+  {
+    if (chunk.Pointer == pointer)
+    {
+      break;
+    }
+    idx++;
+  }
+
+  NF_ASSERT( idx != m_UsedMemoryChunks.Size(), "Could not find Allocated Block" );
+
+  BlockAllocatorChunk chunk = m_UsedMemoryChunks[ idx ];
+  m_UsedMemoryChunks.RemoveAt( idx );
+
+  if (!TryMergeBlocks(chunk))
+  {
+    m_FreeMemoryChunks.Add( chunk );
+  }
+}
+
+nfe::uint64 nfe::BlockAllocator::FindFirstFitBlock( uint64 size )
+{
+  uint64 idx = 0;
+  uint64 firstIdx = -1;
+  for( auto chunk : m_FreeMemoryChunks )
+  {
+    if (chunk.Size >= size)
+    {
+      firstIdx = idx;
+      break;
+    }
+    idx++;
+  }
+
+  return idx;
+}
+
+nfe::uint64 nfe::BlockAllocator::FindBestFitBlock( uint64 size )
+{
+  uint64 bestidx = -1;
+  uint64 idx = 0;
+  for( auto chunk : m_FreeMemoryChunks )
+  {
+    if( chunk.Size >= size )
+    {
+      if (bestidx != -1)
+      {
+        if (m_FreeMemoryChunks[bestidx] > chunk)
+        {
+          bestidx = idx;
+        }
+      }
+      else
+      {
+        bestidx = idx;
+      }
+    }
+    idx++;
+  }
+
+  return bestidx;
+}
+
+nfe::uint64 nfe::BlockAllocator::FindWorstFitBlock( uint64 size )
+{
+
+  uint64 worstIdx = -1;
+  uint64 idx = 0;
+  for( auto chunk : m_FreeMemoryChunks )
+  {
+    if( chunk.Size >= size )
+    {
+      if( worstIdx != -1 )
+      {
+        if( m_FreeMemoryChunks[ worstIdx ] < chunk )
+        {
+          worstIdx = idx;
+        }
+      }
+      else
+      {
+        worstIdx = idx;
+      }
+    }
+    idx++;
+  }
+
+  return worstIdx;
+}
+
+void nfe::BlockAllocator::SplitBlock(uint64 size, BlockAllocatorChunk& firstHalf, BlockAllocatorChunk& secondHalf )
+{
+  if (firstHalf.Size == size)
+  {
+    secondHalf.Pointer = nullptr;
+  }
+  else
+  {
+    uint64 originalSize = firstHalf.Size;
+    firstHalf.Size = size;
+    firstHalf.CalculateEndAddress();
+    secondHalf.Pointer = firstHalf.EndAddress;
+    secondHalf.Size = originalSize - size;
+    secondHalf.CalculateEndAddress();
+  }
+}
+
+bool nfe::BlockAllocator::TryMergeBlocks( const BlockAllocatorChunk& mergeChunk )
+{
+  uint64 preAllocatorIdx = -1;
+  uint64 postAllocatorIdx = -1;
+  uint64 idx = 0;
+  for( auto& chunk : m_FreeMemoryChunks )
+  {
+    if( chunk.EndAddress == mergeChunk.Pointer )
+    {
+      preAllocatorIdx = idx;
+      if( postAllocatorIdx != -1 )
+      {
+        break;
+      }
+    }
+    else if( chunk.Pointer = mergeChunk.EndAddress )
+    {
+      postAllocatorIdx = idx;
+      if( preAllocatorIdx != -1 )
+      {
+        break;
+      }
+    }
+    idx++;
+  }
+
+  if (preAllocatorIdx != -1)
+  {
+    if (postAllocatorIdx != -1)
+    {
+      BlockAllocatorChunk& preBlock = m_FreeMemoryChunks[ preAllocatorIdx ];
+      preBlock.Size += mergeChunk.Size + m_FreeMemoryChunks[ postAllocatorIdx ].Size;
+      preBlock.CalculateEndAddress();
+
+      m_FreeMemoryChunks.RemoveAt( postAllocatorIdx );
+      return true;
+    }
+    else
+    {
+      BlockAllocatorChunk& preBlock = m_FreeMemoryChunks[ preAllocatorIdx ];
+      preBlock.Size += mergeChunk.Size;
+      preBlock.CalculateEndAddress();
+      return true;
+    }
+  }
+  else if (postAllocatorIdx != -1)
+  {
+    BlockAllocatorChunk& postBlock = m_FreeMemoryChunks[ postAllocatorIdx ];
+    postBlock.Pointer = mergeChunk.Pointer;
+    postBlock.Size += mergeChunk.Size;
+    postBlock.CalculateEndAddress();
+
+    return true;
+  }
+
+  return false;
+}
+
+bool nfe::BlockAllocatorChunk::operator==( const BlockAllocatorChunk& chunk ) const
+{
+  return Pointer == chunk.Pointer && Size == chunk.Size;
+}
+
+void nfe::BlockAllocatorChunk::CalculateEndAddress()
+{
+  EndAddress = Pointer + Size;
+}
+
+bool nfe::BlockAllocatorChunk::operator<=( const BlockAllocatorChunk& chunk ) const
+{
+  return Size <= chunk.Size;
+}
+
+bool nfe::BlockAllocatorChunk::operator>=( const BlockAllocatorChunk& chunk ) const
+{
+  return Size >= chunk.Size;
+}
+
+bool nfe::BlockAllocatorChunk::operator<( const BlockAllocatorChunk& chunk ) const
+{
+  return Size < chunk.Size;
+
+}
+
+bool nfe::BlockAllocatorChunk::operator>( const BlockAllocatorChunk& chunk ) const
+{
+  return Size > chunk.Size;
+
+}
+
+bool nfe::BlockAllocatorChunk::Valid()
+{
+  return Pointer != nullptr;
 }
