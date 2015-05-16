4b73a5739421d20d8c5b98b3a803d171435037bb
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-05-16 11:42:15 +0100

    Added Logging interface to NiFrameEngine

---
+++ NiFrameEngine/NiFrameEngine/inc/Logging/Logging.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "NFEnginePCH.hpp"
+
+namespace nfe
+{
+  enum class LogLevel : int32
+  {
+    Fatal,
+    Error,
+    Warning,
+    Success,
+    Log,
+    Info,
+    Debug
+  };
+
+  enum class LogVerbosity : int32
+  {
+    None,
+    Fatal,
+    Error,
+    Warning,
+    Success,
+    Log,
+    Info,
+    Debug,
+    All
+  };
+
+  class NIFRAME_DLL_EXPORT Logger
+  {
+  public:
+    Logger();
+    ~Logger();
+
+  private:
+
+  };
+}
