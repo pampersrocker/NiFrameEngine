3fd82bedcba30335b7f8eb9318add511a058d20b
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-05-21 15:26:35 +0100

    Added description to the ProxyAllocator

--- NiFrameEngine/NiFrameEngine/inc/Memory/NFProxyAllocator.hpp
+++ NiFrameEngine/NiFrameEngine/inc/Memory/NFProxyAllocator.hpp
@@ -14,12 +14,21 @@ namespace nfe
     uint64 Size;
   };
 
+  /**
+  @brief This allocator just passes the allocation request through to the parentAllocator, it does not have any own allocation logic
+
+  However it does track allocations and deallocation for debugging and statistic purpose.
+  */
   class ProxyAllocator : public IAllocator
   {
   public:
     ProxyAllocator( IAllocator* parentAllocator = nullptr, const char* name = "NFProxyAllocator" );
     ~ProxyAllocator();
 
+    /**
+    @brief Returns detailed information of allocation which went through this allocator
+
+    */
     Vector<ProxyAllocationInfo> AllocationInfos() const;
 
     virtual void* Allocate( uint64 size ) override;
