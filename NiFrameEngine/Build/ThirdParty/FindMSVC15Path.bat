62cafe1f57feb681cfc98a46e7c11bcc94143208
Marvin Pohl <pohlm@hdm-stuttgart.de>
2018-05-03 16:49:01 +0200

    Added parts of the compiler setup.

---
+++ NiFrameEngine/Build/ThirdParty/FindMSVC15Path.bat
@@ -0,0 +1,9 @@
+@echo off
+
+for /f "usebackq tokens=1* delims=" %%i in (`vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+	set VS2017DIR=%%i
+)
+set /p VCTOOLSVERSION=<"%VS2017DIR%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
+
+set MSVC15DIRX64="%VS2017DIR%\VC\Tools\MSVC\%VCTOOLSVERSION%\bin\HostX64\x64\"
+set MSVC15DIRX86="%VS2017DIR%\VC\Tools\MSVC\%VCTOOLSVERSION%\bin\HostX64\x86\"
